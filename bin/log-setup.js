0d6c6d5d8868424a2eabc3f1efeec400f19a6706
Mark Stemm <mstemm@jut.io>
2016-01-08 14:51:21 -0800

    Add cmdline args for daemonizing and log control.
    New command line argument -d daemonizes outriggerd.
    
    When daemonized, outriggerd logs to /var/log/outriggerd.log, or
    whatever log is specified by -o <logfile>.
    
    If not daemonized, outriggerd logs to the console.
    
    Remove the built-in log4js.json file--the logging config is now built
    on the fly based on the value of the above options.
    
    You can specify a complete log4js config via -l <log4js-path>.
    
    Update README to reflect these new command line args.

--- bin/log-setup.js
+++ bin/log-setup.js
@@ -2,12 +2,35 @@ var fs = require('fs-extra');
 var path = require('path');
 var log4js = require('log4js');
 
-/**
- * make a log directory, just in case it isn't there.
- */
-module.exports.init = function() {
-    fs.emptyDirSync(path.join(__dirname, '../log'));
+module.exports.init = function(opts) {
+    if (opts.l) {
+        log4js.configure(opts.l);
+    } else {
+        var log4js_opts;
 
-    log4js.configure(path.join(__dirname, '../log4js.json'));
+        if (opts.d) {
+            var logfile = '/var/log/outriggerd.log';
+            if (opts.o) {
+                logfile = opts.o;
+            }
+
+            // Throw an error if we can't log to the configured log
+            // file.
+            fs.accessSync(logfile, fs.R_OK | fs.W_OK);
+
+            log4js_opts = {
+                appenders: [
+                    {type: 'file', filename: logfile}
+                ]
+            };
+        } else {
+            log4js_opts = {
+                appenders: [
+                    {type: 'console'}
+                ]
+            };
+        }
+        log4js.configure(log4js_opts);
+    }
 }
 
