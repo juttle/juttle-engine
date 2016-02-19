8905f10eb1dcad2b1f507be3e6dc4b709262d1f2
Mark Stemm <mstemm@jut.io>
2016-02-19 12:51:20 -0800

    Add events to aggregations table.
    Add columns to the aggregations table to cover the fields that are in
    event points.

--- examples/aws-cloudwatch/create_aws_db.sql
+++ examples/aws-cloudwatch/create_aws_db.sql
@@ -7,6 +7,9 @@ CREATE TABLE IF NOT EXISTS aws_aggregation (
        aggregate VARCHAR(128),
        demographic VARCHAR(128),
        metric_type VARCHAR(128),
+       event_type VARCHAR(128),
+       msg VARCHAR(128),
+       item VARCHAR(128),
        product VARCHAR(128),
 
        INDEX aws_metrics_idx (time)
