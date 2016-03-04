f1df845fffb426d2da7fe52a6266b9e22e7be44e
Mark Stemm <mstemm@jut.io>
2016-03-04 13:11:58 -0800

    Make poll_aws.juttle a shell script.
    Rename poll_aws.juttle.skip to poll_aws.sh so it won't be exposed in
    the file explorer.
    
    Also, move the sleep/echo into that script.

---
+++ examples/aws-cloudwatch/poll_aws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This juttle program does periodic reads of aws information and writes the results to a mysql database.
+
+sleep 40;
+
+echo 'Polling from AWS and Writing to Mysql...';
+
+juttle -e "
+
+import 'https://github.com/juttle/juttle-aws-adapter/raw/master/aws_module.juttle' as AWS;
+
+read aws -from :now: -to :end: -every :10s:
+     | AWS.aggregate_all
+     | write mysql -table 'aws_aggregation';
+
+" && tail -f /dev/null
